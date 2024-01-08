d1ac9b69b69bb706265ff13e2681143629e2878b
eugenioenko <eugenioenko@gmail.com>
2024-01-07 19:47:09 -0800

    feat: mango cli version output

--- mango.go
+++ mango.go
@@ -6,9 +6,23 @@ import (
 	"os"
 )
 
+var (
+	version = "0.3.0"
+)
+
 func main() {
 
 	args := os.Args
+
+	if len(args) == 1 {
+		help()
+		return
+	}
+
+	if args[1] == "version" {
+		fmt.Println(version)
+	}
+
 	if args[1] == "help" {
 		help()
 		return
@@ -25,6 +39,7 @@ Mango is a minimal expression parser and interpreter
 Usage:
 
   mango [expression]: evaluates the expression
+	mango version: prints the version
 	mango help: prints this message
 
 `)
